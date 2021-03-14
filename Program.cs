db2d4c740946212ccc89f7b71eedb9b64cb6b9df
juhriber <j.de.s.r.25@gmail.com>
2021-03-14 18:02:11 -0300

    Finalização do projeto VelocMedia

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace VelocMedia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int distancia;
+            int tempo;
+            int velocidade;
+
+            Console.Write("Digite seu Valor Distancia: ");
+            distancia = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite seu Valor Tempo: ");
+            tempo = int.Parse(Console.ReadLine());
+            Console.Write("\n");
+
+            velocidade = distancia / tempo;
+            Console.WriteLine("Sua Velocidade Média é = {0} m/s", velocidade);
+            Console.Write("\n");
+
+            Console.WriteLine("Pressione ENTER para sair!");
+            Console.ReadKey();
+        }
+    }
+}
